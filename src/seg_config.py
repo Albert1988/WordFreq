f24c029688e09729ae9cbb3da508c342035a3398
xiebingqing <db812074402@126.com>
2015-11-18 13:42:54 +0800

    Revert "V1.0"
    This reverts commit 74c9b9faa2960ece50277986e82014897917c200.

--- src/seg_config.py
+++
@@ -1,8 +0,0 @@
-in_filename=r"E:\SolutionModel\WordFreq\data\P.txt"
-
-indexes=[0]
-
-out_filename=r"E:\SolutionModel\WordFreq\data\P_seg.txt"
-
-str_splitTag=" "
-
--- src/wordfreq_config.py
+++
@@ -1,7 +0,0 @@
-corpus_path = r"E:\SolutionTemp\WordFreq\data\P_seg.txt"
-
-start_index = 0
-
-freq_path = r'E:\SolutionTemp\WordFreq\result\P_freq.txt'
-
-stop_words_path = r'E:\SolutionTemp\WordFreq\dependence\stopwords.txt'
